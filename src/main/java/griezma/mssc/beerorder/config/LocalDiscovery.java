35a5cba4b1e4ec493504280ccf2c4b4f12a29c5a
griezma <griezma@outlook.com>
2021-01-19 21:05:16 +0100

    service discovery added

---
+++ src/main/java/griezma/mssc/beerorder/config/LocalDiscovery.java
@@ -0,0 +1,12 @@
+package griezma.mssc.beerorder.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Profile("localdiscovery")
+@Configuration
+@EnableDiscoveryClient
+public class LocalDiscovery {
+}
